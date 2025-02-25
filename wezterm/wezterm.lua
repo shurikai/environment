98722054b8f3dbcdd0780e553fa97e3bf51a1b1b
Jason Chestnut <pauperboards@gmail.com>
2025-02-24 22:45:15 -0500

    getting ready to rework main.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,50 +1,130 @@
 local wezterm = require("wezterm")
 
-local config = wezterm.config_builder()
+local config = {}
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
 
-config = {
-	automatically_reload_config = true,
-	enable_tab_bar = false,
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE",
-	default_cursor_style = "BlinkingBar",
-	--color_scheme = 'tokyonight',
-	-- color_scheme = "Grayscale (dark) (terminal.sexy)",
-	color_scheme = "kanagawabones",
-	-- color_scheme = "nord",
-	-- font = wezterm.font("M+CodeLat50 Nerd Font", { weight = "Regular" }),
-	font = wezterm.font("IosevkaTermSlab Nerd Font", { weight = "Regular" }),
-	-- font = wezterm.font("IosevkaTerm Nerd Font", { weight = "Light" }),
-	font_size = 18,
+local act = wezterm.action
 
-	background = {
-		-- {
-		-- 	source = {
-		-- 		File = "/Users/x318323/.config/wezterm/background.jpg",
-		-- 	},
-		-- 	hsb = {
-		-- 		hue = 1.0,
-		-- 		saturation = 1.0,
-		-- 		brightness = 0.3,
-		-- 	},
-		-- 	width = "100%",
-		-- 	height = "100%",
-		-- },
-		{
-			source = {
-				Color = "#282c35",
-			},
-			width = "100%",
-			height = "100%",
-			-- opacity = 0.90,
+config.automatically_reload_config = true
+
+config.color_scheme = "kanagawabones"
+
+config.colors = {
+	tab_bar = {
+		active_tab = {
+			bg_color = "#938AA9",
+			fg_color = "#DCD7BA",
 		},
-	},
-	window_padding = {
-		left = 14,
-		right = 14,
-		top = 14,
-		bottom = 14,
+		-- inactive_tab = {},
+		inactive_tab_hover = {
+			bg_color = "#16161D",
+			fg_color = "#54546D",
+			italic = false,
+		},
+		-- new_tab = {},
+		-- new_tab_hover = {},
 	},
 }
+
+config.font = wezterm.font("IosevkaTermSlab Nerd Font", { weight = "Regular" })
+config.font_size = 18
+-- config.font = wezterm.font("IosevkaTerm Nerd Font", { weight = "Light" })
+-- config.font = wezterm.font("M+CodeLat50 Nerd Font", { weight = "Regular" })
+-- config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.default_cursor_style = "BlinkingBar"
+
+config.use_fancy_tab_bar = false
+
+config.scrollback_lines = 3000
+
+config.inactive_pane_hsb = {
+	-- saturation = 0.5,
+	brightness = 0.5,
+}
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	-- Send C-a when pressing C-a twice.
+	{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
+
+	-- Pane-related bindings
+	{ key = "s", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "v", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+	{ key = "q", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+	-- Set up a special set of keybindings to handle resizing of panes.
+	{
+		key = "r",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+	},
+
+	-- Tab-related bindings
+	{ key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "n", mods = "LEADER", action = act.ShowTabNavigator },
+
+	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+}
+
+config.key_tables = {
+	resize_pane = {
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+}
+
+wezterm.on("update-status", function(window, pane)
+	local stat = window:active_workspace()
+	local scheme = wezterm.color.get_builtin_schemes()[config.color_scheme]
+
+	if window:active_key_table() then
+		stat = window:active_key_table()
+	end
+	if window:leader_is_active() then
+		stat = "LEADER"
+	end
+
+	local basename = function(s)
+		return string.gsub(s, "(.*[/\\])(.*)", "%2")
+	end
+
+	-- Current command
+	local cmd = pane:get_foreground_process_name()
+	-- CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
+	cmd = cmd and basename(cmd) or ""
+
+	window:set_left_status(wezterm.format({
+		{ Foreground = { Color = "#B98D7B" } },
+		{ Text = "  " .. wezterm.nerdfonts.oct_table .. "  " .. stat },
+		{ Text = " " },
+	}))
+
+	local time = wezterm.strftime("%l:%M%P")
+
+	window:set_right_status(wezterm.format({
+		{ Foreground = { Color = "#D27E99" } },
+		{ Text = cmd .. " " .. wezterm.nerdfonts.cod_terminal .. " " },
+		{ Foreground = { Color = "#727169" } },
+		{ Text = " | " },
+		{ Foreground = { Color = "#7E9CD8" } },
+		{ Text = time .. " " .. wezterm.nerdfonts.md_clock },
+		{ Text = " " },
+	}))
+end)
 
 return config
